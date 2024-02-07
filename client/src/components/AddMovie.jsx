e4bc375ef1296a8f6f20af9238f9dc0ea591e847
Styles <stylesk11@gmail.com>
2024-02-07 12:07:44 -0800

    stub add movie component

---
+++ client/src/components/AddMovie.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AddMovie = (props) => {
+
+
+  return (
+    <form>
+      <input type='text' id='addMovieField' placeholder='Add movie here...'/>
+    </form>
+  )
+
+
+}
+
+export default AddMovie;
