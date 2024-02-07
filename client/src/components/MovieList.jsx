bcdb45e192e987bf3fb40b0ec0a1fcce4edd41c4
Styles <stylesk11@gmail.com>
2024-02-07 11:15:13 -0800

    use entry component on movie list

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -1,15 +1,18 @@
+import React from 'react';
+import MovieListEntry from '/client/src/components/MovieListEntry.jsx';
+
 const MovieList = ({ movies }) => {
-
-
+  console.log({movies});
 
   return (
     <ul>
-      {movies.map((movie) => {
+      {movies.map((movie) => (
         <MovieListEntry movie={movie} />
-      })}
-
+      ))}
     </ul>
 
   )
 
 }
+
+export default MovieList;
