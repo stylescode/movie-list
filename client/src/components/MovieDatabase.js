4fc14e1c074df275a0dda5a6fccebe1ca2922d1c
Styles <stylesk11@gmail.com>
2024-02-07 10:23:47 -0800

    add default movies to list

---
+++ client/src/components/MovieDatabase.js
@@ -0,0 +1,9 @@
+const MovieDatabase = [
+  { title: 'Mean Girls' },
+  { title: 'Hackers' },
+  { title: 'The Grey' },
+  { title: 'Sunshine' },
+  { title: 'Ex Machina' },
+];
+
+export default MovieDatabase;
