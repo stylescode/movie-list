c72a6ebb216ea9ab3b1b571269fd14f3ccb90556
Styles <stylesk11@gmail.com>
2024-02-07 12:01:53 -0800

    add handleSearch function

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 
-import MovieDatabase from '/client/src/components/MovieDatabase.js'
+import MovieDatabase from '/client/src/components/MovieDatabase.js';
+import Search from '/client/src/components/Search.jsx';
+import MovieList from '/client/src/components/MovieList.jsx';
 
-const App = (props) => {
+
+const App = () => {
 
   const [movies, setMovies] = React.useState(MovieDatabase);
 
+  const handleSearch = (e, input) => {
+    e.preventDefault();
+    console.log(input);
+    // filter movies from database based on input
+    let filteredMovies = MovieDatabase.filter((movie) => {
+      return movie.title.toLowerCase().includes(input.toLowerCase());
+    })
+    // use setMovies to update movies
+    setMovies(filteredMovies);
+
+  }
+
 
   return (
-    <div>Hello World!</div>
+    <div>
+      <Search handleSearch={handleSearch}/>
+      <MovieList movies={movies} />
 
+    </div>
 
   )
 
