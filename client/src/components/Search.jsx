d9f2f6ad340192d0d5caab29705549911e148765
Styles <stylesk11@gmail.com>
2024-02-07 17:28:23 -0800

    pass handleSearch prop from App

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
-const Search = ({ handleSearch }) => {
-  return (
+const Search = ({ handleSearch }) => (
     <form>
       <input type='text' id='searchField' placeholder='Search...'/>
-      <button onClick={(e) => handleSearch(e, document.getElementById('searchField').value)}>GO!</button>
+      <button onClick={(e) => handleSearch(e, document.getElementById('searchField').value)}>Go</button>
     </form>
-
-  )
-}
+)
 
 export default Search;
